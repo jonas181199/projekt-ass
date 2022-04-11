af0f258dffebe1df5abe444e4b1c30d062705354
jonas181199 <73401789+jonas181199@users.noreply.github.com>
2022-04-11 19:32:41 +0200

    s

---
+++ Markt.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<HTML>
+   <HEAD>
+      <!-- HTML-Kopf -->
+      <meta charset=“utf-8“>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Getränkemarkt-Registrierung</title>
+   </HEAD>
+   <BODY>
+      <!-- HTML-Körper -->
+        <button type=”submit” name=”submit”> Getränke erfassen</button>
+        <button type=”submit” name=”submit”> Lagerverwaltung</button>
+        <button type=”submit” name=”submit”> Auswertung</button>
+   </BODY>
+</HTML>
---
+++ Marktanmeldung.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<HTML>
+   <HEAD>
+      <!-- HTML-Kopf -->
+      <meta charset=“utf-8“>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Getränkemarkt-Anmeldung</title>
+   </HEAD>
+   <BODY>
+      <!-- HTML-Körper -->
+      <form method=“POST“ action=“loginScript.php”>
+        Markt-ID: <input type=”text” name=”userName” />
+        <br> <br>
+        Kennwort: <input type=”password” name=”password” />
+        <br> <br>
+        <a href="Markt.php"> <button type=”submit” name=”submit”> Anmelden</button> </a> 
+        
+        <br> <br>
+        <a href=""> Registrieren</a> 
+        </form>        
+   </BODY>
+</HTML>
