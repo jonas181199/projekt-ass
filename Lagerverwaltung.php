c52fe126aec1637e822e0300183028f2bb283a7e
jonas181199 <73401789+jonas181199@users.noreply.github.com>
2022-04-11 21:08:30 +0200

    sss

--- Lagerverwaltung.php
+++ Lagerverwaltung.php
@@ -5,10 +5,9 @@
       <meta charset=“utf-8“>
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Erfolgreiche Getränkemarkt-Registrierung</title>
+      <title>Lagerverwaltung</title>
    </HEAD>
    <BODY>
       <!-- HTML-Körper -->
-      <p>Ihr Markt wurde erfolgreich erstellt.</p>
    </BODY>
 </HTML>
--- Markt.php
+++ Markt.php
@@ -5,12 +5,26 @@
       <meta charset=“utf-8“>
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Getränkemarkt-Registrierung</title>
+      <title>Die Möglichkeiten im Portal</title>
    </HEAD>
    <BODY>
       <!-- HTML-Körper -->
-        <button type=”submit” name=”submit”> Getränke erfassen</button>
-        <button type=”submit” name=”submit”> Lagerverwaltung</button>
-        <button type=”submit” name=”submit”> Auswertung</button>
+      <h2>Die Möglichkeiten in Ihrem Markt-Portal</h2>
+      <form action="Getraenkeerfassen.php" method="POST">
+         <p>
+         <input type="submit" value="Getraenke erfassen">
+         </p>
+      </form>
+      <form action="Lagerverwaltung.php" method="POST">
+         <p>
+         <input type="submit" value="Lagerverwaltung">
+         </p>
+      </form>
+      <form action="Getraenkeerfassen.php" method="POST">
+         <p>
+         <input type="submit" value="Auswertung">
+         </p>
+      </form>     
+
    </BODY>
 </HTML>
