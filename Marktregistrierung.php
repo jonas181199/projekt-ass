2661f7beac67184c3702f5bcbe9f6f98c58e7981
jonas181199 <73401789+jonas181199@users.noreply.github.com>
2022-04-11 19:32:07 +0200

    initial

---
+++ Marktregistrierung.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML>
+   <HEAD>
+      <!-- HTML-Kopf -->
+      <meta charset=“utf-8“>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Getränkemarkt-Registrierung</title>
+   </HEAD>
+   <BODY>
+      <!-- HTML-Körper -->
+      <form method=“POST“ action=“loginScript.php”>
+        Markt-ID: <input type=”text” name=”userName” />
+        <br> <br>
+        Marktname: <input type=”text” name=”marktName” />
+        <br> <br>
+        Kennwort: <input type=”password” name=”password” />
+        <br> <br>
+        <button type=”submit” name=”submit”> Registrieren</button>
+        </form>        
+   </BODY>
+</HTML>
