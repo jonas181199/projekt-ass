d718a84538c10f11d1b5775bc20b7d65fd3ffc31
Schirm Jonas <jonas.schirm@puma.com>
2022-05-05 15:25:12 +0200

    auswertung

--- Auswertung.php
+++ Auswertung.php
@@ -13,5 +13,28 @@
    </HEAD>
    <BODY>
       <!-- HTML-Körper -->
+      <h1>Auswertung</h1>
+      <form action="ErfolgteAuswertung.php" method="POST" >
+         <fieldset>
+            <legend>Bitte geben Sie den Starttermin für die Auswertung an</legend>
+            <p>
+               <label for="start">Starttermin </label>
+               <input type="date" name="start" id="start">
+            </p>
+            <p>
+               <input type="submit" name="auswerten" value="Auswertung starten">
+            </p>
+         </fieldset>
+      </form>
+      
+
+      
+               <p>
+                  <a href="KategorieAuswertung.php">Zur Auswertung nach Kategorie</a> 
+               </p>
+
+
+
+
    </BODY>
 </HTML>
