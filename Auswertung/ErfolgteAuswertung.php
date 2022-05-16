37d56f1f28cf27326f52d0e82ddfb436d9bc6d8d
noahschoene <schoene.noah@gmail.com>
2022-05-16 12:55:48 +0200

    Jahresding

--- Auswertung/ErfolgteAuswertung.php
+++ Auswertung/ErfolgteAuswertung.php
@@ -13,59 +13,80 @@
     <BODY>
         <?php
             $akJahr  = idate('Y');
-            $akWoche = idate('W');
-            $ewoche  = idate('W', strtotime("05/08/2022"));
+            $akWoche = date('W');
+            $ewoche  = date('W', strtotime($_POST['start']));
             $data[][] = null;
 
-            for($i = $ewoche; $i <= $akWoche; $i++){
-                $timestamp_montag  = strtotime("{$akJahr}-W{$i}");
-                $timestamp_sonntag = strtotime("{$akJahr}-W{$i}-7");
-                
-                $data[0]['KW'] = $i;
+            // Jahresgrenzending
+            $eJahr = date("Y", strtotime($_POST['start']));
+            if (date("m", strtotime($_POST['start'])) == "01" && (date("W", strtotime($_POST['start'])) == 52 || date("W", strtotime($_POST['start'])) == 53))
+                $eJahr--;
+            else if (date("m", strtotime($_POST['start'])) == "12" && date("W", strtotime($_POST['start'])) == 01)
+                $eJahr++;
 
-                //Gesamtumsatz
-                $sqlgu = "SELECT SUM(g.preis) FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag";                       
-                $resultgu = $conn->query($sqlgu);
-                $sgu = $resultgu->fetch_object();
-                $data[0]['Gesamtumsatz'] = $sgu;
 
-                //Größte Bestellung
-                $sqlgb = "SELECT SUM(g.preis) FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr HAVING MAX(SUM(g.preis))";                       
-                $resultgb = $conn->query($sqlgb);
-                $sgb = $resultgb->fetch_object();
-                $data[0]['Größte Bestellung'] = $sgb;
+            for($j =  $eJahr; $j <= $akJahr; $j++)  {
 
-                //Standartabweichung
-                $avg = "SELECT AVG FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
-                $anz = "SELECT count * FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
-                $preise = "SELECT g.preis FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
-                $abw = 0;
-
-                $resultst = $conn->query($preise);
-                while($sst = $resultst->fetch_object()){
-                    $abw += ($sst->preis - $avg) * ($sst->preis - $avg);
+                if($j < $akJahr) {
+                    $anzW = idate('W', mktime(0, 0, 0, 12, 28, $j));
+                } elseif($j = $akJahr)  {
+                    $anzw = $akWoche;
                 }
-                $result = sqrt( (1/($anz-1)) * $abw);
-                $data[0]['Standartabweichung'] = $result;
-                
-                //Median
-                $sqlm = "SELECT SUM(g.preis) FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag )";                       
-                $resultm = $conn->query($sqlm);
-                $sm = $resultm->fetch_object();
 
-                $preisa[] = $resultst->fetch_assoc(); 
+                for($i = $ewoche; $i <= $anzW; $i++){
 
-                $mittelwert = floor(($anz-1)/2); 
-                if($anz % 2) { 
-                    $median = $preisa[$mittelwert];
-                } else { 
-                    $low = $preisa[$mittelwert];
-                    $high = $preisa[$mittelwert+1];
-                    $median = (($low+$high)/2);
+                    $timestamp_montag  = strtotime("{$akJahr}-W{$i}");
+                    $timestamp_sonntag = strtotime("{$akJahr}-W{$i}-7");
+                    
+                    $data[0]['KW'] = $i;
+
+                    //Gesamtumsatz
+                    $sqlgu = "SELECT SUM(g.preis) FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag";                       
+                    $resultgu = $conn->query($sqlgu);
+                    $sgu = $resultgu->fetch_object();
+                    $data[0]['Gesamtumsatz'] = $sgu;
+
+                    print_r($data);
+
+                    //Größte Bestellung
+                    $sqlgb = "SELECT SUM(g.preis) as summe, b.bestellnr FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr HAVING MAX(SUM(g.preis))";                 
+                    $resultgb = $conn->query($sqlgb);
+                    $sgb = $resultgb->fetch_object();
+                    $data[0]['Größte Bestellung'] = $sgb->summe;
+
+                    //Standartabweichung
+                    $avg = "SELECT AVG FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
+                    $anz = "SELECT count * FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
+                    $preise = "SELECT g.preis FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag GROUP BY b.bestellnr";                       
+                    $abw = 0;
+
+                    $resultst = $conn->query($preise);
+                    while($sst = $resultst->fetch_object()){
+                        $abw += ($sst->preis - $avg) * ($sst->preis - $avg);
+                    }
+                    $result = sqrt( (1/($anz-1)) * $abw);
+                    $data[0]['Standartabweichung'] = $result;
+                    
+                    //Median
+                    $sqlm = "SELECT SUM(g.preis) FROM bestellpos bp, bestellung b, getraenke g where bp.bestellnr = b.bestellnr AND bp.ghersteller = g.ghersteller AND bp.gname = g.gname AND b.bestdatum >= $timestamp_montag AND b.bestdatum >= $timestamp_sonntag )";                       
+                    $resultm = $conn->query($sqlm);
+                    $sm = $resultm->fetch_object();
+
+                    $preisa[] = $resultst->fetch_assoc(); 
+
+                    $mittelwert = floor(($anz-1)/2); 
+                    if($anz % 2) { 
+                        $median = $preisa[$mittelwert];
+                    } else { 
+                        $low = $preisa[$mittelwert];
+                        $high = $preisa[$mittelwert+1];
+                        $median = (($low+$high)/2);
+                    }
+                    $data[0]['Median'] = $median;
+
+                    print_r($data);
                 }
-                $data[0]['Median'] = $median;
-
-                print_r($data);
+                $ewoche = 1;
             }
         ?>
 
