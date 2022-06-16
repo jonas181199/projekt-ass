fc78d6c81d350a16d1282cbd4d4e21455b47f3af
Julian-alber <77838864+Julian-alber@users.noreply.github.com>
2022-06-16 13:39:53 +0200

    Markt objektorientiert

--- Anmeldung/Markt.php
+++ Anmeldung/Markt.php
@@ -1,21 +1,66 @@
 <?php
    include_once '../includes/dbh.inc.php';
+   include_once '../classes/anmeldungMarktKl.php';
+   include_once '../classes/registrierungMarktKl.php';
    session_start();
 
-   if (empty($_SESSION['mid']) AND !isset($_POST['loginmarkt']) AND !isset($_POST['loginmarkt'])) {
+   if (empty($_SESSION['mid']) AND !isset($_POST['loginmarkt']) AND !isset($_POST['registrieremarkt'])) {
       header('Location: Marktanmeldung.php');
       exit;
    }
    
-   // ganze Prüfungen hier einfügen (methoden Aufrufe)
 
-   if(!isset($_SESSION['mid']) OR isset($_POST['loginmarkt']) OR isset($_POST['loginmarkt'])){
+   if (isset($_POST['loginmarkt'])){ 
+      $mid = mysqli_real_escape_string($conn, $_POST['mid']);
+      $markt = new anmeldungMarktKl($mid, $_POST['mpasswort'], $conn);
+   
+      if (!$markt->alleFelderBelegt()){
+         echo "Bitte füllen Sie die erforderlichen Felder aus!";
+         return;
+      }  
+      if (!$markt->midPruefen()){
+         echo "Die Markt-ID existiert nicht!";
+         return;
+      }  
+      if (!$markt->passwortPruefen()){
+         echo "ID und Passwort stimmen nicht überein!";
+         return;
+      }  
+   }
+   elseif(isset($_POST['registrieremarkt'])){
+      $mid          = mysqli_real_escape_string($conn, $_POST['mid']);
+      $mname        = mysqli_real_escape_string($conn, $_POST['mname']);
+      $mpasswort_un = mysqli_real_escape_string($conn, $_POST['mpasswort']);
+      $mpasswort    = password_hash($mpasswort_un, PASSWORD_BCRYPT);
+
+      $markt = new registrierungMarktKl($mid, $mname, $_POST['mpasswort'], $conn);
+      
+      if (!$markt->alleFelderBelegt()){
+         echo "Bitte füllen Sie die erforderlichen Felder aus!";
+         return;
+      }  
+      if (!$markt->midPruefen()){
+         echo "Die Markt-ID ist bereits vergeben!";
+         return;
+      }  
+      if (!$markt->mnamePruefen()){
+         echo "Der Marktname ist bereits vergeben!";
+         return;
+      }  
+      if (!$markt->marktHinzufuegen()){
+         echo "Fehler beim Erstelen ihres Marktes";
+         return;
+      } 
+      echo "Ihr Markt wurde erfolgreich erstellt.";
+   }
+
+   
+   if(!isset($_SESSION['mid']) OR isset($_POST['loginmarkt']) OR isset($_POST['registrieremarkt'])){
       $_SESSION['mid'] = $_POST['mid'];
    } 
 ?>
 
 <!-- Dies ist eine gemeinsame Leistung der Gruppe -->
-
 <!DOCTYPE HTML>
 <HTML>
    <HEAD>
@@ -26,89 +71,6 @@
       <title>Die Möglichkeiten im Portal</title>
    </HEAD>
    <BODY>
-      <?php
-      if (isset($_POST['loginmarkt'])){
-         $mid = mysqli_real_escape_string($conn, $_POST['mid']);
-
-         //Prüfen, ob alle Felder befüllt
-         if(!isset($_POST['mid']) || strlen($_POST['mid']) == 0 || 
-            !isset($_POST['mpasswort']) || strlen($_POST['mpasswort']) == 0){
-               echo "Bitte füllen Sie die erforderlichen Felder aus!";
-               return;
-         } 
-   
-         //Prüfen, ob Markt-ID existiert
-         function check_id(): bool {
-            $db = new mysqli ("localhost", "root", "", "getraenkeshop_ass");
-            $mids = $db->query("select mid from markt");
-            while(($s = $mids->fetch_object()) != false){
-               if($s->mid == $_POST['mid']){
-                  // echo "Die Markt-ID existiert!";
-                  return true;
-               }
-            }
-            return false;
-         }
-         
-         if (check_id() == false){
-            echo "Die Markt-ID existiert nicht!";
-            return;
-         }
-         
-
-         //Prüfen, ob Markt-ID und Passwort übereinstimmen
-         $markt = $conn->query("select mpasswort from markt where mid = '$mid'");
-         $s = $markt->fetch_object();
-         if(password_verify($_POST['mpasswort'], $s->mpasswort) == false){
-            echo "ID und Passwort stimmen nicht überein!";
-            return;
-         } 
-      }
-      elseif(isset($_POST['registrieremarkt'])){
-         $mid          = mysqli_real_escape_string($conn, $_POST['mid']);
-         $mname        = mysqli_real_escape_string($conn, $_POST['mname']);
-         $mpasswort_un = mysqli_real_escape_string($conn, $_POST['mpasswort']);
-         $mpasswort    = password_hash($mpasswort_un, PASSWORD_BCRYPT);
-
-         //Prüfen, ob alle Felder befüllt
-         if(!isset($_POST['mid']) || strlen($_POST['mid']) == 0 || 
-            !isset($_POST['mname']) || strlen($_POST['mname']) == 0 || 
-            !isset($_POST['mpasswort']) || strlen($_POST['mpasswort']) == 0){
-               echo "Bitte füllen Sie die erforderlichen Felder aus!";
-               return;
-         } 
-      
-         //Prüfen, ob Markt-ID schon vergeben
-         $mids = $conn->query("select mid from markt");
-         while(($s = $mids->fetch_object()) != false){
-            if($s->mid == $_POST['mid']){
-               echo "Die Markt-ID ist bereits vergeben!";
-               return;
-            }
-         }
-
-         //Prüfen, ob Marktname schon vergeben
-         $mn = $conn->query("select mname from markt");
-         while(($s = $mn->fetch_object()) != false){
-            if($s->mname == $_POST['mname']){
-               echo "Der Marktname ist bereits vergeben!";
-               return;
-            }
-         }
-
-         $sql = "insert into markt values ('" . $mid. "', '" . $mpasswort. "', '" . $mname. "')";
-         if ($conn->query($sql) == false){
-            echo "fehler";
-            echo $conn->error;
-         }
-         else {
-            echo "Ihr Markt wurde erfolgreich erstellt.";
-         }
-         $conn->close();
-      }
-
-      ?>
-
       <h2>Die Möglichkeiten in Ihrem Markt-Portal</h2>
       <form action="../Getraenk/Getraenkeerfassen.php" method="POST">
          <p>
