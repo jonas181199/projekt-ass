ae20d544f0174332151fe9496359123b7f369b35
Schirm Jonas <jonas.schirm@puma.com>
2022-06-16 11:23:07 +0200

    Create insertgetraenke.sql

---
+++ procedures_functions/insertgetraenke.sql
@@ -0,0 +1,22 @@
+<!-- Jonas Schirm -->
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `insertgetraenk`(`p_ghersteller` VARCHAR(50), `p_gname` VARCHAR(30), `p_kategorie` ENUM('Wasser','Saft','Bier','Wein','Limonade','Sonstiges'), `p_preis` DECIMAL(10,2)) RETURNS int(11)
+BEGIN
+
+DECLARE EXIT HANDLER FOR 1062
+BEGIN
+	return 2;
+END;
+
+IF (p_preis > 0.00) THEN
+	INSERT into getraenke (ghersteller, gname, kategorie, preis) VALUES (p_ghersteller, p_gname, p_kategorie, p_preis);
+	return 0;
+END IF;
+IF (p_preis <= 0.00) THEN
+	return 1;
+END IF;
+
+
+
+END$$
+DELIMITER ;
