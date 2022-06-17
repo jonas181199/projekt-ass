05f325a5d57d650ece56707ec1166c0e09708305
Schirm Jonas <jonas.schirm@puma.com>
2022-06-17 11:10:44 +0200

    s

--- Auswertung/ErfolgteVorhersage.php
+++ Auswertung/ErfolgteVorhersage.php
@@ -3,24 +3,28 @@
     session_start();
     include_once '../classes/auswertung.php';
     include_once '../includes/dbh.inc.php';
+    if ((empty($_SESSION['mid']))) {
+        header('Location: ../Anmeldung/Marktanmeldung.php');
+        exit;
+    }
 ?>
 
 <?php
 if(isset($_POST['vorhersage'])){
     $eingabe_datum = date('Y-m-d');
     $eingabe_datum_arr = explode('-', $eingabe_datum);
-    $auswertung_kat = $_POST['kategorie'];
+    $kategorie = $_POST['kategorie'];
 
     $akJahr   = idate('Y');
 	$akWoche  = date('W');
     $timestamp_montag  = date("Y-m-d", strtotime("{$akJahr}-W{$akWoche}"));
     $timestamp_sonntag = date("Y-m-d", strtotime("{$akJahr}-W{$akWoche}-7")); 
 
-    $startdatum_time = mktime(0,0,0,$eingabe_datum_arr[1],$eingabe_datum_arr[2],$eingabe_datum_arr[0]);
-    $auswertung = new Auswertung($startdatum_time, $auswertung_kat, $_SESSION['mid'], $conn);
+    $startdatum_time = mktime(0, 0, 0, $eingabe_datum_arr[1], $eingabe_datum_arr[2], $eingabe_datum_arr[0]);
+    $auswertung = new Auswertung($startdatum_time, $kategorie, $_SESSION['mid'], $conn);
     $umsaetze = $auswertung->getGesamtumsatz($timestamp_montag, $timestamp_sonntag);
     $regressionswert = $auswertung->lineareRegression_calc($umsaetze);
     ?>
 
-    <p>Erwarteter Umsatz für die kommende Woche: <?php echo $regressionswert ?> </p>
+    <p> <h3>Erwarteter Umsatz für die kommende Woche:</h3> <?php echo $regressionswert?> EUR </p>
 <?php } ?>
